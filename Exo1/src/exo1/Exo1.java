e4bfd615a2d7d6fdb44ff495769953c0653bb3b8
Edess <Edess@10.3.60.169>
2024-09-20 17:53:11 +0200

    Exo 1 terminé

--- Exo1/src/exo1/Exo1.java
+++ Exo1/src/exo1/Exo1.java
@@ -4,17 +4,32 @@
  */
 package exo1;
 
+import java.util.Scanner;
+
+
 /**
- *
- * @author Edess
- */
+Noms : PINCEDE Edesse
+Groupe : TDB 
+Rôle du programme : TP0
+Date : 20/09/2024**/
 public class Exo1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       
+   System.out.println("Bonjour");
+        int maVariable = 20;
+        maVariable=20;
+        maVariable=maVariable+1;
+         System.out.println("Au revoir");
+          String prenom;
+ Scanner sc;
+ sc = new Scanner(System.in);
+ System.out.println("Bonjour, dites moi quel est votre prenom ?");
+ prenom = sc.nextLine();
+    
+    }  
     
 }
