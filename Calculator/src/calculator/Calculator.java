c4e3caf6a796bac389d663f878e56731f27ec6d2
Edess <Edess@10.3.60.169>
2024-09-20 18:34:38 +0200

    Calculator terminé

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -4,6 +4,8 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
 Noms : PINCEDE Edesse
 Groupe : TDB 
@@ -15,7 +17,47 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    System.out.println("1)Add \n2)Susbtract \n3)Multiply \n4)Divide \n5)Modulo");
+     int operateur;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("\n Entrer l'operateur que vous souhaitez utiliser :");
     
+     operateur=sc.nextInt();
+      if (operateur<1)
+          System.out.println("Erreur");
+           System.exit(0);
+       if (operateur>5)
+          System.out.println("Erreur");
+           System.exit(0);
+      
+     
+
+ 
+ 
+  System.out.println("\n Entrer une première valeur :");
+  int operande1;
+  operande1=sc.nextInt();
+  
+    System.out.println("\n Entrer une deuxième valeur :");
+  int operande2;
+  operande2=sc.nextInt();
+  
+  if (operateur == 1){
+      System.out.println("Le resultat vaut "+(operande1 + operande2)+"");
+      
+    }
+  if (operateur == 2){
+      System.out.println("Le resultat vaut "+ (operande1-operande2)+"");
+      
+    }
+   if (operateur == 3){
+      System.out.println("Le resultat vaut "+ (operande1*operande2)+"");
+    }
+     if (operateur == 4){
+      System.out.println("Le resultat vaut "+ (operande1*operande2)+"");
+    }
+       if (operateur == 5){
+      System.out.println("Le resultat vaut "+ (operande1%operande2)+"");
+    }
+  }
 }
