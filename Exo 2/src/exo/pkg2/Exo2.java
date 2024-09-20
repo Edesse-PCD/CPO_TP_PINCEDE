b084ab9ad3ec53dc8c2235e1ddb6fd1b47823058
Edess <Edess@10.3.60.169>
2024-09-20 17:53:47 +0200

    Exo 2 terminé

--- Exo 2/src/exo/pkg2/Exo2.java
+++ Exo 2/src/exo/pkg2/Exo2.java
@@ -1,20 +1,41 @@
-/*
+/**
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package exo.pkg2;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Edess
- */
+Noms : PINCEDE Edesse
+Groupe : TDB 
+Rôle du programme : TP0
+Date : 20/09/2024**/
 public class Exo2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
+ //Declaration des variables
+ int nb;
+ //nb=5;
+ Scanner sc = new Scanner(System.in);
+ System.out.println("\n Entrer le nombre :");
+ nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+ int result; // resultat
+ int ind; //indice
+ result=0;
+ // Addition des nb premiers entiers
+ ind=1;
+ while (ind <= nb) {
+ result=result+ind;
+ ind+=1;
+ }
+ // Affichage du resultat
+ System.out.println();
+ System.out.println("La somme des "+ nb + "entiers est: "+result);
+ }
 }
+    
