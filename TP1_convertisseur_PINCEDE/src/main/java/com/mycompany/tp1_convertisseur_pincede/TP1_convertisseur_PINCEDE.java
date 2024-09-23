3ccc6d69a3146719dd4067be9c375b9d0f9d5c97
Edess <Edess@10.3.60.169>
2024-09-23 21:47:55 +0200

    Exercice 2 TP1 terminé

--- TP1_convertisseur_PINCEDE/src/main/java/com/mycompany/tp1_convertisseur_pincede/TP1_convertisseur_PINCEDE.java
+++ TP1_convertisseur_PINCEDE/src/main/java/com/mycompany/tp1_convertisseur_pincede/TP1_convertisseur_PINCEDE.java
@@ -16,25 +16,25 @@ public static double CelciusVersKelvin (double tCelcius){
     tCelcius+= 273;
     return tCelcius;
 }
-public static double FareinheitVersCelcius(double tFareinheit){
-    tFareinheit = (tFareinheit-32)/1.8;
-    return tFareinheit;
+public static double FarenheitVersCelcius(double tFarenheit){
+    tFarenheit = (tFarenheit-32)/1.8;
+    return tFarenheit;
 }
 
-public static double CelciusVersFareinheit (double tCelcius){
+public static double CelciusVersFarenheit (double tCelcius){
     tCelcius = (tCelcius*9/5)+32;
     return tCelcius;
 }
 
-public static double KelvinVersFareinheit (double tCelcius){
-    tCelcius+= -273+CelciusVersFareinheit(tCelcius) ;
+public static double KelvinVersFarenheit (double tCelcius){
+    tCelcius+= -273+CelciusVersFarenheit(tCelcius) ;
     return tCelcius;
 }
 
 
-public static double FareinheitVersKelvin (double tFareinheit){
-    tFareinheit+= +273+ FareinheitVersCelcius(tFareinheit);
-    return tFareinheit;
+public static double FarenheitVersKelvin (double tFarenheit){
+    tFarenheit+= +273+ FarenheitVersCelcius(tFarenheit);
+    return tFarenheit;
 }
 
 
@@ -46,19 +46,28 @@ System.out.println("Bonjour, saisissez une valeur:\n");
 double valeur = sc.nextDouble(); // je crée ma variable nommée valeur, l'utilisateur lui attribue une variable
 System.out.println(valeur); // j'affiche cette valeur 
 
-System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n1) De Celcius vers Kelvin \n2) De Kelvin vers Celcius  \n3)CelciusVersFarenheit() \n4)KelvinVersFarenheit() \n5)FarenheitVersKelvin() \n6) De Farenheit vers Kelvin" );
-
+System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n1)De Celcius vers Kelvin \n2)De Kelvin vers Celcius  \n3)De Celcius vers Farenheit \n4)De Kelvin vers Farenheit \n5)De Farenheit vers Kelvin \n6)De Farenheit vers Kelvin" );
+int conversion = sc.nextInt();
 /*2)
 double valeur_K = valeur + 273; // je crée ma nouvelle valeur qui convertir les celsius en kelvin
 System.out.println(valeur_K); // je l'affiche 
 
 
-//3) Cette fois ci, j'utilise la méthode que j'ai crée 
+//3) Cette fois ci, j'utilise la méthode que j'ai créée
 
 System.out.println("La valeur convertit en kelvin vaut "+CelciusVersKelvin(valeur)+"");
 */
 
+// A present je vais crer un algorithme permettant de déterminer comment fomruler la phrase 
 
+switch (conversion){ //J'utilise switch pour alléger mon code 
+    case 1: System.out.println(""+valeur+" degrés Celcius est égal à "+CelciusVersKelvin(valeur)+" degres Kelvin");break; //Je prends la valeur et je la convertis en fonction de ce que demande l'utilisateur 
+    case 2: System.out.println(""+valeur+" degrés Farenheit est égal à "+FarenheitVersCelcius(valeur)+" degres Celcius");break; //Je répète l'opération pour chaque conversions.
+    case 3: System.out.println(""+valeur+" degrés Celcius est égal à "+CelciusVersFarenheit(valeur)+" degres Farenheit");break;
+    case 4: System.out.println(""+valeur+" degré Kelvin est égal à "+KelvinVersFarenheit(valeur)+" degres Farenheit");break;
+    case 5: System.out.println(""+valeur+" degré Farenheit est égal à "+FarenheitVersKelvin(valeur)+" degres Kelvin");break;
+}
+        
 
 
         
