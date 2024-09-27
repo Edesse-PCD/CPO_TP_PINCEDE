2ac43cba3310227b8123cbd1c33aaa059d4f90f2
Edess <Edess@10.3.60.169>
2024-09-27 10:23:39 +0200

    Classe BouteilleBiere TP2

---
+++ TP2_Bieres_PINCEDE/src/main/java/com/mycompany/tp2_bieres_pincede/BouteilleBiere.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp2_bieres_pincede;
+
+/**
+ *
+ * @author Edess
+ */
+public class BouteilleBiere {
+public void lireEtiquette() { 
+System.out.println("Bouteille de " + nom +" (" + degreAlcool + 
+" degres) \nBrasserie : " + brasserie ) ; 
+}
+
+    String nom;
+    float degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    
+BouteilleBiere uneBiere = new BouteilleBiere() ;
+    uneBiere.nom="Cuvée des trolls";
+
+    
+    
+    
+    
+}
