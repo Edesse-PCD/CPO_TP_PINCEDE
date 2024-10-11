302091ec8feb74fc920351206e3b8b23956e7d5a
Edess <Edess@10.3.60.169>
2024-10-11 09:06:44 +0200

    TP2 exo2 en cours

---
+++ TP2_convertisseurObjet_Pincede/src/tp2_convertisseurobjet_pincede/Convertisseur.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_pincede;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Edess
+ */
+public class Convertisseur {
+    int nbConversions = 0;
+
+    public double CelciusVersKelvin (double tCelcius){
+    tCelcius+= 273;
+    nbConversions+=1;
+     return tCelcius;
+}
+public  double FarenheitVersCelcius(double tFarenheit){
+    tFarenheit = (tFarenheit-32)/1.8;
+    nbConversions+=1;
+    return tFarenheit;
+}
+
+public double CelciusVersFarenheit (double tCelcius){
+    tCelcius = (tCelcius*9/5)+32;
+    nbConversions+=1;
+    return tCelcius;
+}
+
+public double KelvinVersFarenheit (double tCelcius){
+    tCelcius+= -273+CelciusVersFarenheit(tCelcius) ;
+    nbConversions+=1;
+    return tCelcius;
+}
+
+
+public double FarenheitVersKelvin (double tFarenheit){
+    tFarenheit+= +273+ FarenheitVersCelcius(tFarenheit);
+    nbConversions+=1;
+    return tFarenheit;
+}
+
+@Override 
+public String toString () { 
+return "nb de conversions"+ nbConversions;
+}
+
+
+
+    
+}
---
+++ TP2_convertisseurObjet_Pincede/src/tp2_convertisseurobjet_pincede/TP2_convertisseurObjet_Pincede.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_pincede;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Edess
+ */
+public class TP2_convertisseurObjet_Pincede {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     Scanner sc = new Scanner(System.in);    
+    Convertisseur Convertisseur1 = new Convertisseur();
+    
+    Convertisseur1.CelciusVersKelvin (10);
+    System.out.println(Convertisseur1.nbConversions);
+    }
+    
+}
