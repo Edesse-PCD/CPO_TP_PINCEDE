6e0dc12bc5e57ea162e814d0c5826cbf87643170
Edess <Edess@10.3.60.169>
2024-10-23 10:24:48 +0200

    TP2 exo 4 début

---
+++ TP2_relation_1_PINCEDE_Edesse_CARRERA_Valentin/src/tp2_relation_1_pincede_edesse_carrera_valentin/Voiture.java
@@ -0,0 +1,14 @@
+package tp2_relation_1_pincede_edesse_carrera_valentin;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Edess
+ */
+public class Voiture {
+    
+}
