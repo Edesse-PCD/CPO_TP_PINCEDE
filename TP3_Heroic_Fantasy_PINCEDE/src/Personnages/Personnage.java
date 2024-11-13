acdc2115a925777a4eb2c19ddf9bd8aafb8af6b6
Edess <Edess@10.3.60.169>
2024-11-13 17:14:17 +0100

    TP3 suite

---
+++ TP3_Heroic_Fantasy_PINCEDE/src/Personnages/Personnage.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personnages;
+import java.util.ArrayList;
+import Armes.Arme;
+/**
+ *
+ * @author Edess
+ */
+abstract public class Personnage {
+    String nom;
+    int vie; 
+
+    public Personnage(String nom, int vie) {
+        this.nom = nom;
+        this.vie = vie;
+    }
+
+    @Override
+    public String toString() {
+        return "Personnage{" + "nom=" + nom + ", vie=" + vie + '}';
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getVie() {
+        return vie;
+    }
+    
+ //3 Gestion des armes par le personnages 
+     //3.1 Ajouter gestion des armes par les personnages 
+    
+     //31
+     ArrayList<Arme>  Sac_à_dos  = new ArrayList<Arme>();
+     
+     //32 
+     public void Arme_perso(Arme arme){
+         
+         if (Sac_à_dos.size()<=5){
+             Sac_à_dos.add(arme);      
+         }
+         
+          arme_portée = NULL
+         
+     }
+}
